5bc72a18a5edd17c223edba7f2090c257664c841
Adam Hemming <hemming.dev@gmail.com>
2022-08-09 14:28:51 +0900

    allow user to edit tasks

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'tasks/new', to: 'tasks#new', as: :new_task
   post 'tasks', to: 'tasks#create', as: :tasks
   get 'tasks', to: 'tasks#index'
-
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
   get 'tasks/:id', to: 'tasks#show', as: :task
 
 end
