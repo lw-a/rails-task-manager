7710143cc99b2150cc35d971d0b19a0db75d2bd9
Adam Hemming <hemming.dev@gmail.com>
2022-08-09 14:20:31 +0900

    allow user to add a task

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'tasks', to: 'tasks#index', as: :tasks
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create', as: :tasks
+  get 'tasks', to: 'tasks#index'
 
   get 'tasks/:id', to: 'tasks#show', as: :task
+
 end
